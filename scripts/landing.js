d6e8aae8409b1c1ece0f469746b62beb6fe3b885
wolffles <tofukillzstevent@live.com>
2017-03-06 10:31:21 -0800

    commit assignment8 #2

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,9 +1,13 @@
-var revealPoint = function(){
+
+var animatePoints = function() {
     var points = document.getElementsByClassName('point');
-    for(var i = 0; i < points.length; i++){
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-}
+    var revealPoint = function(idx){
+            points[idx].style.opacity = 1;
+            points[idx].style.transform = "scaleX(1) translateY(0)";
+            points[idx].style.msTransform = "scaleX(1) translateY(0)";
+            points[idx].style.WebkitTransform = "scaleX(1) translateY(0)";
+        };
+     for(var i = 0; i < points.length; i++){
+         revealPoint(i)
+    };
+};
