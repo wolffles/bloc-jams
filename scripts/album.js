295a17132896b4e80bfcc9afd4f44e8691db3f1e
wolffles <tofukillzstevent@live.com>
2017-03-13 16:03:14 -0700

    checkpoint13 help

--- scripts/album.js
+++ scripts/album.js
@@ -117,11 +117,17 @@ var setCurrentAlbum = function(album) {
      }
  };
 
+var getSongItem = function(){
+    
+};
+
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
+var currentlyPlayingSong = null;
  window.onload = function() {
     var list = [albumPicasso, albumMarconi, albumFood];
     var n = 0;
@@ -135,7 +141,6 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
     });
      
     songListContainer.addEventListener('mouseover', function(event){
-        //1
         //console.log(event.target);
         
         // Only target individual song rows during event delegation
@@ -144,14 +149,70 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
         }
     });
+    songListContainer.addEventListener('click', function(event){
+        //alert(this.className);
+        //change the content form the number to the play button's HTML
+        event.target.parentElement.querySelector('.song-item-number').innerHTML = pauseButtonTemplate;
+        currentlyPlayingSong = this.querySelector('.song-item-number').getAttribute('data-song-number');
+        // used this to test currentlyplayingsong value
+        //console.log(currentlyPlayingSong === this.children[0].getAttribute('data-song-number'));
+        //console.log(currentlyPlayingSong);
+        //console.log(this.querySelector('.song-item-number').getAttribute('data-song-number'));
+        //console.log(this.querySelector('.song-item-number').textContent.value);
+        //console.log(this.querySelectorAll(".song-item-number"));
+    
+    });
+    /* 
+    songListContainer.addEventListener('mouseleave', function(event){
+        //console.log(event.target);
+        
+        // Only target individual song rows during event delegation
+        if (event.target.parentElement.className === 'album-view-song-item' ) {
+            //change the content form the number to the play button's HTML
+            console.log(event.target.parentElement.querySelector('.song-item-number').innerHTML);
+        }
+    });
+    */
+     
     for(var i = 0; i< songRows.length; i++) {
+        
         songRows[i].addEventListener('mouseleave', function(event) {
             // Revert the content back to the number
+            //if(currentlyPlayingSong == this.querySelector('.song-item-number').getAttribute('data-song-number')){
             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+            // };
         });
     }
  };
 
 
+/* functionality goals
 
+1. When we click a song to play it, the song number should 
+change to a pause button:
+
+2.When the mouse leaves the table row of the currently playing
+song, the pause button should remain:
+
+3.When we switch songs, the previously playing song's table 
+cell should revert the content back to the song number:
+
+4.When we hover over each of the songs that aren't playing, 
+the play button should still appear; we don't, however, want 
+to show the play button when we hover over the playing song:
+*/
+
+
+/*sudo code
+
+create a click EventListener to change the play button 
+to a pause button
+
+stop the autoloop of mouseleave for loop on 165, to only work
+non pause button elements. turn it into a case statement
+
+only when we click on another item will the pause button change.
+
+set an exemption to the play button mouse over event.
+*/
 
